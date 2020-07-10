788169adde214db020ed0928276933973387642a
longluuu <longlu_@hotmail.com>
2020-07-10 15:15:16 +0100

    service worker and manifest

---
+++ build/precache-manifest.dc50af41e2d9df2f9634330ab6b335ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0ab9f2cef3099c3184cc65afc2dbb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0448d89e40e16e96a11b",
+    "url": "/static/css/main.db892025.chunk.css"
+  },
+  {
+    "revision": "702e2b0e2a3e2d5c3ae9",
+    "url": "/static/js/2.c2b3b82a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2b3b82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0448d89e40e16e96a11b",
+    "url": "/static/js/main.84328bb2.chunk.js"
+  },
+  {
+    "revision": "10cd3d79c12bb7fc2ad3",
+    "url": "/static/js/runtime-main.74310779.js"
+  },
+  {
+    "revision": "7406c940b11d4f7ca52901e39dd9124b",
+    "url": "/static/media/hero-image.7406c940.jpg"
+  },
+  {
+    "revision": "c69956292f11efbe92720aa5abfd827d",
+    "url": "/static/media/taha-mazandarani-eLTEuvma_vw-unsplash.c6995629.jpg"
+  }
+]);
